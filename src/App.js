dcc922a69b33fbee18c373e489deb06b80fe8818
李賢成 <leesmac@likenchengui-MacBookAir.local>
2023-08-07 23:59:56 +0900

    #7.3 Movie App part One

--- src/App.js
+++ src/App.js
@@ -1,54 +1,32 @@
 import {useState, useEffect} from "react";
 
 function App() {
-    const [loding, setLoding] = useState(true);
-    const [coins, setCoins] = useState([]);
-    const [myMoney, setMyMoney] = useState(0);
-    const [exCoin, setExCoin] = useState(0);
-
-    useEffect(() => {
-      fetch("https://api.coinpaprika.com/v1/tickers")
-      .then(response => response.json())
-      .then(json => {
-        setCoins(json);
-        setLoding(false);
-        if(json && json[0]){
-          setExCoin(json[0].quotes.USD.price);
-        }
-      });
-    },[]);
-
-    const onChange = (event) => {
-      setMyMoney(event.target.value);
-    };
-
-    const onChangeSelect = (event) => {
-      console.log(event.target.value);
-      setExCoin(event.target.value);
-    }
-
-    console.log(`myMoney : ${myMoney}`);
-    console.log(`exCoin : ${exCoin}`);
-
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const geMovies = async () => {
+    const json =  await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+        )).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  useEffect(() => {
+    geMovies();
+  }, []);
   return (
-    <div>
-      <h1>The Coins {loding?"": `(${coins.length})`}</h1>
-      {loding ? <strong>Loding...</strong> : (
-        <form>
-          <input type="number" onChange={onChange} placeholder="money that you want to exchange" value={myMoney} />
-          <select onChange={onChangeSelect}>
-          {coins.map((coin) => (
-            <option value={coin.quotes.USD.price} key={coin.id} >
-              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-        <hr/>
-        <h3>Your Coins : {myMoney && myMoney!=="0" ?(myMoney/exCoin).toFixed(2):""}</h3>
-        </form>
-      ) }
-      
-    </div>
+    <div>{loading?<h1>"Loading..."</h1> : <div>{movies.map((movie) => 
+        <div key={movie.id}>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <h2>{movie.title}</h2>
+          <p>{movie.summary}</p>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+              ))}
+          </ul>
+        </div>
+    )}</div>}</div>
   );
 }
 
