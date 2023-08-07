c578bea0134e3bf834a2c354d7a1b67eabfb67d5
李賢成 <leesmac@likenchengui-MacBookAir.local>
2023-08-07 22:13:37 +0900

    코인을 달러로 자동계산기능 추기

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
     const [loding, setLoding] = useState(true);
     const [coins, setCoins] = useState([]);
     const [myMoney, setMyMoney] = useState(0);
+    const [exCoin, setExCoin] = useState(0);
 
     useEffect(() => {
       fetch("https://api.coinpaprika.com/v1/tickers")
@@ -11,6 +12,9 @@ function App() {
       .then(json => {
         setCoins(json);
         setLoding(false);
+        if(json && json[0]){
+          setExCoin(json[0].quotes.USD.price);
+        }
       });
     },[]);
 
@@ -18,21 +22,29 @@ function App() {
       setMyMoney(event.target.value);
     };
 
+    const onChangeSelect = (event) => {
+      console.log(event.target.value);
+      setExCoin(event.target.value);
+    }
+
+    console.log(`myMoney : ${myMoney}`);
+    console.log(`exCoin : ${exCoin}`);
+
   return (
     <div>
       <h1>The Coins {loding?"": `(${coins.length})`}</h1>
       {loding ? <strong>Loding...</strong> : (
         <form>
-          <input type="text" onChange={onChange} placeholder="money that you want to exchange" value={myMoney} />
-          <select>
+          <input type="number" onChange={onChange} placeholder="money that you want to exchange" value={myMoney} />
+          <select onChange={onChangeSelect}>
           {coins.map((coin) => (
-            <option>
+            <option value={coin.quotes.USD.price} key={coin.id} >
               {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
         <hr/>
-        <h3>Your money : {myMoney}</h3>
+        <h3>Your Coins : {myMoney && myMoney!=="0" ?(myMoney/exCoin).toFixed(2):""}</h3>
         </form>
       ) }
       
