3b3f3ac79464cff770a490f205d893ed5f6a9a7b
李賢成 <leesmac@likenchengui-MacBookAir.local>
2023-08-10 00:24:00 +0900

    #7.5 React Router

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,21 @@
-import Proptypes from "prop-types"
+import Proptypes from "prop-types";
+import {Link} from "react-router-dom";
 
 function Movie({coverImage, title, summary, genres}){
-    return (<div>
-    <img src={coverImage} alt={title} />
-    <h2>{title}</h2>
-    <p>{summary}</p>
-    <ul>
-      {genres.map((g) => (
-        <li key={g}>{g}</li>
-        ))}
-    </ul>
-  </div>)
+    return (
+      <div>
+        <img src={coverImage} alt={title} />
+        <h2>
+          <Link to="/movie">{title}</Link>
+        </h2>
+        <p>{summary}</p>
+        <ul>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
+    );
 }
 
 Movie.propTypes = {
